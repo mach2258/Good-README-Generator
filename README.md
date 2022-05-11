40f542e5cd4b5da2413f4ab14b938180836f8053
Erich Elste <erichelste@yahoo.com>
2022-05-10 22:10:35 -0400

    readme generator working

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-# 
-    http://github.com//
-    # Description 
-    
-    # Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Liscense](#liscense)
-    * [Constributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    #Installation
-    Listed below are the dependencies that must be installed to run the project.
-    #Usage
-    The usage of this application is, undefined
-    #Liscense
-    The Liscense used for the project is  liscense.
-    ![Github liscense](http://img.shields.io/badge/liscense-MIT-blue-svg)
-    #Contributing
-    Contributors to the project include: 
-    #Tests
-    The following command is required to test run the project: 
-    #Questions
-    If you have any questions about the project contact  
+# Good-README-Generator
+http://github.com/mach2258/Good-README-Generator
+# Description 
+Creates a readme.md based on user input
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Liscense](#liscense)
+* [Constributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+# Installation
+Listed below are the dependencies that must be installed to run the project.
+# Usage
+The usage of this application is, undefined
+# Liscense
+The Liscense used for the project is MIT liscense.
+![Github liscense](http://img.shields.io/badge/liscense-MIT-blue-svg)
+# Contributing
+Contributors to the project include: Erich Elste
+# Tests
+The following command is required to test run the project: node index.js
+# Questions
+If you have any questions about the project contact mach2258 erichelste@yahoo.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,30 +12,30 @@ function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.Title}
-    http://github.com/${data.Username}/${data.Title}
-    # Description 
-    ${data.Description}
-    # Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Liscense](#liscense)
-    * [Constributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    #Installation
-    Listed below are the dependencies that must be installed to run the project.
-    #Usage
-    The usage of this application is, ${data.Usage}
-    #Liscense
-    The Liscense used for the project is ${data.Liscense} liscense.
-    ![Github liscense](http://img.shields.io/badge/liscense-MIT-blue-svg)
-    #Contributing
-    Contributors to the project include: ${data.Contributing}
-    #Tests
-    The following command is required to test run the project: ${data.Tests}
-    #Questions
-    If you have any questions about the project contact ${data.Username} ${data.Email}
+return `# ${data.Title}
+http://github.com/${data.Username}/${data.Title}
+# Description 
+${data.Description}
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Liscense](#liscense)
+* [Constributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+# Installation
+Listed below are the dependencies that must be installed to run the project.
+# Usage
+The usage of this application is, ${data.Usage}
+# Liscense
+The Liscense used for the project is ${data.Liscense} liscense.
+![Github liscense](http://img.shields.io/badge/liscense-MIT-blue-svg)
+# Contributing
+Contributors to the project include: ${data.Contributing}
+# Tests
+The following command is required to test run the project: ${data.Tests}
+# Questions
+If you have any questions about the project contact ${data.Username} ${data.Email}
 `;
 }
 
